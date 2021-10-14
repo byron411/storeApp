9a1066c78b4e162ebc9866bb6fed59dc7347235b
unknown <byron789_@hotmail.com>
2021-10-14 14:27:33 -0500

    Organizar react

---
+++ src/app/components/login.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Login extends Component{
+    render(){
+        return(
+            <h1>Este es Login</h1>
+        )
+    }
+}
+export default Login;
--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,9 @@
+//Se necesita para cargar html
 import React, {Component} from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 
-class App extends Component{
-    render(){
-        return(
-            <h1>Hola mundo desde react</h1>
-        )
-    }
-}
-render(<App />, document.getElementById('app'));
+import App from './App';
+
+
+
+render (<App />, document.getElementById('app'));
